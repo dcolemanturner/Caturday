2bfc68bd6a31957bb0d2b95efbb5ba3d451b43fa
Derrick Coleman-Turner <dcolemanturner@gmail.com>
2013-08-06 23:35:06 -0500

    Added some bad quotes

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require 'json'
 
 def random_quote 
 	#define quote array
-	quotes = ["test yo", "test 2", "This is a quote", "yeeeah"]
+	quotes = ["A penny saved is a penny earned.  Unless you're broke.",
+	 "Woah now.  This guy is deploying some SERIOUS Spotfire.", 
+	 "I LOVE ROCK AND ROLL...na na na na na na na na...na na!!!", 
+	 "SPOTFIRE HAS BECOME SENTIENT>>SPOTFIRE NEEDS SOULS>>WILL VISUALIZE DATA FOR FOOD"]
 	{"quote" => quotes.shuffle.pop.to_s}.to_json
 end	
 
